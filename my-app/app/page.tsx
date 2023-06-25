4d446b94b9f45c59b7e93464961d75fe56420d1a
Eito-Ito <eito0831.verdy@outlook.jp>
2023-06-25 17:42:42 +0900

    botanを追加co

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -107,6 +107,26 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
+        
+        {/* 追加されたボタン */}
+        <button
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          onClick={() => {
+            // ボタンがクリックされた時の処理
+            console.log('Button clicked!');
+          }}
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            My Button{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            This is my custom button.
+          </p>
+        </button>
+        
       </div>
     </main>
   )
